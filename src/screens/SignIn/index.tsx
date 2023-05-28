e3d7a76bedb0a847a6a23c77875ea025b3f9db05
Mateus Anderle <mateusanderle@Mateuss-MacBook-Pro.local>
2023-05-28 19:00:13 -0300

    first commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
+import * as S from "./styles";
+
+import backgroundImg from "../../assets/background.png";
+import { Button } from "../../components/Button";
+
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from "@env";
+import { Alert } from "react-native";
+
+WebBrowser.maybeCompleteAuthSession();
+
+export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [_, response, googleSignIng] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ["profile", "email"],
+  });
+
+  function handleGoogleSignIn() {
+    setIsAuthenticating(true);
+
+    googleSignIng().then((response) => {
+      if (response?.type !== "success") {
+        setIsAuthenticating(false);
+      }
+    });
+  }
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      if (response.authentication?.idToken) {
+      } else {
+        Alert.alert(
+          "Entrar",
+          "Não foi possível conectar-se a sua conta google."
+        );
+        setIsAuthenticating(false);
+      }
+    }
+  }, [response]);
+
+  return (
+    <S.Container source={backgroundImg}>
+      <S.Title>Ignite Fleet</S.Title>
+
+      <S.Slogan>Gestão de uso de veículos</S.Slogan>
+
+      <Button
+        title="Entrar com Google"
+        onPress={handleGoogleSignIn}
+        isLoading={isAuthenticating}
+      />
+    </S.Container>
+  );
+}
